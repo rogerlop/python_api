049f7bbcbead96ad17158e0530e4faa3755beb08
Roger L <rogermariolopez@gmail.com>
2022-11-23 11:00:02 -0400

    adding dockerfile and requirements

--- app.py
+++ app.py
@@ -1,28 +1,53 @@
+import uuid
 from flask import Flask, request
+from flask_smorest import abort
+from db import items, stores
 
 app = Flask(__name__)
-
-stores = [
-    { 
-        "name": "My Store",
-        "items": [
-            {
-                "name": "Chair",
-                "price": 15.99
-            }
-        ]
-    }
-]
 
 
 @app.get("/store")
 def get_stores():
-    return {"stores": stores}
+    return {"stores": list(stores.values())}
 
 
 @app.post("/store")
 def create_store():
-    request_data = request.get_json()
-    new_store = {"name": request_data["name"], "items": []}
-    stores.append(new_store)
-    return new_store, 201
+    store_data = request.get_json()
+    store_id = uuid.uuid4().hex
+    store = {**store_data, "id": store_id}
+    stores[store_id] = store
+    return store, 201
+
+
+@app.post("/item")
+def create_item(name):
+    item_data = request.get_json()
+    if item_data["store_id"] not in stores:
+        abort(404,message = "Store not found")
+
+    item_id = uuid.uuid4.hex
+    item = {**item_data, "id": item_id}
+    items[item_id] = item 
+
+    return new_item, 201
+
+
+@app.get("/item/<string:store_id>")
+def get_store():
+    return {"items": list(items.values())}
+
+
+@app.get("/item/<string:item_id>")
+def get_item(item_id):
+    try:
+        return items[item_id]
+    except KeyError:
+        abort(404,message = "Store not found")
+    
+
+
+
+
+
+
